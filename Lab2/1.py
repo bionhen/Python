996bf55a423162defe9d1dccede08afc4c777b55
bionhen <90710926+bionhen@users.noreply.github.com>
2021-09-21 12:52:49 +0300

    Add files via upload

---
+++ Lab2/1.py
@@ -0,0 +1,12 @@
+import turtle as t
+from random import *
+t.shape('turtle')
+i=0
+t.speed(50)
+for i in range (0, 400):
+ t.forward(randint(10, 100))
+ t.left(randint(0, 360))
+ t.backward(randint(10, 100))
+ t.right(randint(0, 360))
+ i=i+1
+
