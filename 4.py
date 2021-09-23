eb93f894f71a8b176aa9aad23b88cb2e2dc59060
bionhen <90710926+bionhen@users.noreply.github.com>
2021-09-23 23:44:48 +0300

    Add files via upload

--- 4.py
+++ 4.py
@@ -1,8 +1,27 @@
 import turtle as t
-
-t.shape('turtle')
-i=0
-for i in range (0,360,1):
- t.forward(1)
- t.left(1)
- i=i+1
+import numpy as np
+from random import *
+t.shape('circle')
+t.speed(100)
+x = -300
+y = 0
+dt = 0.1
+Vx = 40
+Vy = 50
+i = 0
+t.penup()
+t.goto(-300, 0)
+t.pendown()
+while i < 1000:
+ if Vy > 0:
+    ay = -10 - 0.05*Vy
+ else: ay = -10 - 0.05*Vy
+ x += Vx*dt
+ y += Vy*dt
+ Vy += ay*dt
+ ax = -0.05*Vx
+ Vx += ax*dt
+ i = i + 1
+ if y < 0:
+  Vy = -Vy
+ t.goto(x, y)
--- 5.py
+++ 5.py
@@ -1,16 +1,32 @@
+from random import randint
 import turtle as t
+import numpy as np
+t.shape('circle')
+t.penup()
+t.goto(-200, -200)
+t.pendown()
+t.speed(50)
+for g in range(4):
+ t.forward(400)
+ t.left(90)
+number_of_turtles = 20
+steps_of_time_number = 1000000
+pool = [t.Turtle(shape='circle') for i in range(number_of_turtles)]
+Vx = [randint(-20,20) for l in range(number_of_turtles)]
+Vy = [((-1)**randint(0, 10))*np.sqrt(400-(Vx[k])**2) for k in range(number_of_turtles)]
+x = [randint(-200, 200) for k in range(number_of_turtles)]
+y = [randint(-200, 200) for k in range(number_of_turtles)]
+print(Vx, Vy)
+for unit in pool:
+    unit.penup()
+    unit.speed(0.5)
+for i in range(steps_of_time_number):
+    for j in range(number_of_turtles):
+        pool[j].goto(x[j], y[j])
+        x[j] += Vx[j]*0.1
+        y[j] += Vy[j]*0.1
+        if x[j] < -200 or x[j] > 200:
+             Vx[j] = -Vx[j]
+        if y[j] < -200 or y[j] > 200:
+             Vy[j] = -Vy[j]
 
-t.shape('turtle')
-j=0
-i=16
-t.goto(0,0)
-while i<160:
- while j<4:
-  t.forward(i)
-  t.left(90)
-  j=j+1
- t.penup()
- t.goto(-i/2, -i/2)
- t.pendown()
- j=0
- i=i+16
