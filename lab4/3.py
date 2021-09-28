3bc32301e45081012f922404676533eb28c93bb6
Bionhen <liubimov.is@phystech.edu>
2021-09-28 23:30:34 +0300

    car2

--- lab4/3.py
+++ lab4/3.py
@@ -6,14 +6,16 @@ color=(255, 255, 255)
 FPS = 30
 screen = pygame.display.set_mode((1500, 800))
 screen.fill(color)
-x = 100; y = 100
-a = 1
 N = 10
-def car[a, x, y]:
-      polygon(screen, (0, 0, 255), [(x, y), (x + 100 * a, y), (x + a * 100, y + 20 * a), (x + a * 180, y + 20 * a),
-(x + a * 180, y + 80 * a), (x - 40 * a, y + 80 * a), (x - 40 * a, y + 20 * a)], (x, y + 20 * a))
-      polygon(screen, (0, 0, 255), [(x+5*a, y+5*a), (x+45*a, y+5*a), ()])
-
+def car(a, x, y):
+      ellipse(screen, (1, 1, 1), (x-50*a, y+65*abs(a), 30*abs(a), 10*abs(a)))
+      polygon(screen, (0, 0, 255), [(x, y), (x+100*a, y), (x+a*100, y+40*abs(a)), (x+a*180, y+40*abs(a)),
+      (x+a*180, y+80*abs(a)), (x-40*a, y+80*abs(a)), (x-40*a, y+40*abs(a)), (x, y+40*abs(a))])
+      polygon(screen, (255, 255, 255), [(x+10*a, y+10*abs(a)), (x+45*a, y+10*abs(a)), (x+45*a, y+40*abs(a)), (x+10*a, y+40*abs(a))])
+      polygon(screen, (255, 255, 255), [(x+65*a, y+10*abs(a)), (x+90*a, y+10*abs(a)), (x+90*a, y+40*abs(a)), (x+65*a, y+40*abs(a))])
+      ellipse(screen, (1, 1, 1), (x-10*a, y+65*abs(a), 40*abs(a), 30*abs(a)))
+      ellipse(screen, (1, 1, 1), (x+130*a, y+65*abs(a), 40*abs(a), 30*abs(a)))
+car(-1, 700, 400)
 pygame.display.update()
 clock = pygame.time.Clock()
 finished = False
