cf4d346ad272dd618f0d0622bb0bb4cbb0afeeea
Bionhen <liubimov.is@phystech.edu>
2021-10-04 15:57:35 +0300

    final

--- lab4/4.py
+++ lab4/4.py
@@ -43,12 +43,12 @@ while i<200:
     m += 1
     n += 1
     k += 4
-    rect(screen, (51, m, n), (-200, k, 1500, 800))
+    rect(screen, (51, 51, 51), (-200, k, 1500, 800))
     i += 1
 rect(screen, (50, 50, 50), (0, 0, 800, 120))
 rect(screen, (204, 204, 204), (500, 20, 150, 500))
 rect(screen, (102, 102, 102), (310, 70, 120, 400))    #smallcenter
-rect(screen, (102, 102, 102), (450, 150, 120, 400))    #smallcenter
+rect(screen, (102, 102, 102), (450, 110, 120, 400))    #smallcenter
 rect(screen, (153, 204, 204), (10, 100, 120, 400))      #smallrl
 rect(screen, (153, 204, 204), (670, 90, 120, 400))  # smallrl
 car(1.0, 270, 600)
@@ -66,25 +66,28 @@ ellipse(circle, (100, 100, 100, 128), (240, 40, 150, 30))
 ellipse(circle, (150, 150, 150, 128), (640, 30, 120, 40))
 rect(circle, (220, 220, 220, 128), (20, 150, 300, 300))
 rect(circle, (220, 220, 220, 128), (500, 120, 300, 300))
+rect(circle, (1, 1, 1, 158), (500, 0, 100, 100))
+rect(circle, (1, 1, 1, 58), (200, 0, 100, 100))
+rect(circle, (1, 1, 1, 150), (0, 0, 100, 100))
 ellipse(circle, (10, 10, 10, 128), (100, 30, 120, 40))
 ellipse(circle, (10, 10, 10, 128), (630, 50, 150, 60))
 ellipse(circle, (10, 10, 10, 128), (430, 40, 300, 70))
 i = 0
-m = 200
-n = 200
-k = 200
-r = 100
-l = 500
-z = 600
-while i < 20:
-    m += 1
-    n += 1
-    k += 1
-    r -= 5
-    l -= 1
-    z -= 1
+m = 51
+n = 51
+k = 51
+r = 300
+l = -500
+z = 500
+while i < 25:
+    m += 6
+    n += 6
+    k += 6
+    r -= 10
+    l += 5
+    z += 5
     i += 1
-    ellipse(circle, (m, n, k, 128), (l, z, r, r))
+    ellipse(circle, (m, n, k, 128), (l, z, 10*r, r))
 screen.blit(circle, (0, 0))
 pygame.display.update()
 clock = pygame.time.Clock()
