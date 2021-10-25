70a97e6adf4efb2a4ea86f400ae039f24ff114e8
Bionhen <liubimov.is@phystech.edu>
2021-10-25 21:53:38 +0300

    Final

--- lab4(Шар)/3.py
+++ lab4(Шар)/3.py
@@ -16,9 +16,14 @@ BLACK = (0, 0, 0)
 COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
 score = 0
 
+# Задает класс Ball с методами:
+# движения move
+# столкновения со стеной collision_wall
+# столкновения с другим шариком collision_ball
+
 
 class Ball:
-    def __init__(self, x, y, vx, vy, r, COLOR, count):
+    def __init__(self, x, y, vx, vy, vx1, vy1, r, COLOR, count):
      self.x = x
      self.y = y
      self.r = r
@@ -26,6 +31,8 @@ class Ball:
      self.vx = vx
      self.vy = vy
      self.COLOR = COLOR
+     self.vx1 = vx1
+     self.vy1 = vy1
 
     def move(self):
        self.x += self.vx
@@ -40,20 +47,22 @@ class Ball:
 
     def collision_ball(self):
       if self.count > 0:
-         self.vx = -self.vx
-         self.vy = -self.vy
+         self.vx = self.vx1
+         self.vy = self.vy1
+
+# Создает кортеж balls из элементов Ball с рандомной длиной
 
 
 balls = []
-for i in range(randint(3, 5)):
-    x = randint(100, 700)
+for i in range(randint(6, 12)):
+    x = randint(100+100*i, 200+100*i)
     y = randint(100, 500)
     r = randint(30, 50)
     vx = randint(-10, 10)
     vy = randint(-10, 10)
     COLOR = COLORS[randint(0, 5)]
     count = 0
-    my_ball = Ball(x, y, vx, vy, r, COLOR, count)
+    my_ball = Ball(x, y, vx, vy, 0, 0, r, COLOR, count)
     balls.append(my_ball)
 
 
@@ -62,7 +71,6 @@ def new_ball(color, x, y, r):
 
 
 l = len(balls)
-
 
 
 def click(event):
@@ -98,15 +106,37 @@ while not finished:
                 vy = randint(-10, 10)
                 COLOR = COLORS[randint(0, 5)]
                 count = 0
-                my_ball = Ball(x, y, vx, vy, r, COLOR, count)
+                my_ball = Ball(x, y, vx, vy, 0, 0, r, COLOR, count)
                 balls.append(my_ball)
+
             print('score:', score)
     for i in range(0, l, 1):
         for j in range(i+1, l, 1):
+            if (balls[j].x-balls[i].x)**2+(balls[j].y-balls[i].y)**2 <= (balls[j].r+balls[i].r)**2:
+                balls[j].count = 1
+                balls[i].count = 1
+                balls[i].vx1 = balls[j].vx
+                balls[i].vy1 = balls[j].vx
+                balls[j].vx1 = balls[i].vx
+                balls[j].vy1 = balls[i].vy
+                Ball.collision_ball(balls[i])
+                Ball.collision_ball(balls[j])
+    for i in range(0, l, 1):
+        for j in range(i+1, l, 1):
             if (balls[j].x-balls[i].x)**2+(balls[j].y-balls[i].y)**2 < (balls[j].r+balls[i].r)**2:
-                balls[j].count += 1
-                balls[i].count += 1
-        Ball.collision_ball(balls[i])
+                balls.pop(i)
+                x = randint(100, 700)
+                y = randint(100, 500)
+                r = randint(30, 50)
+                vx = randint(-10, 10)
+                vy = randint(-10, 10)
+                COLOR = COLORS[randint(0, 5)]
+                count = 0
+                my_ball = Ball(x, y, vx, vy, 0, 0, r, COLOR, count)
+                balls.append(my_ball)
+                balls.append(my_ball)
+
+
     for i in range(l):
         Ball.move(balls[i])
     for i in range(l):
@@ -119,6 +149,8 @@ while not finished:
         Ball.collision_wall(balls[i])
     for i in range(0, l, 1):
         balls[i].count = 0
+        balls[i].vx1 = 0
+        balls[i].vy1 = 0
     pygame.display.update()
     screen.fill(BLACK)
 
