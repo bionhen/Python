3f6f4bcd8997dcecc7f7ee024989447e9feed9ae
Bionhen <liubimov.is@phystech.edu>
2021-11-01 22:21:52 +0300

    Bullets

--- lab5/1.py
+++ lab5/1.py
@@ -17,8 +17,8 @@ WHITE = (255, 255, 255)
 score = 0
 GAME_COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
 
-WIDTH = 800
-HEIGHT = 600
+WIDTH = 1000
+HEIGHT = 800
 
 surf = pygame.Surface((200, 100))
 
@@ -51,11 +51,16 @@ class Ball:
         self.vy += -self.g/30
         self.x += self.vx
         self.y -= self.vy
-        if self.x - self.r < 0 or self.x +self.r > 800:
-            self.vx = -self.vx
-        if self.y - self.r < 0 or self.y +self.r > 600:
-            self.vy = -self.vy
-
+        if self.x - self.r <= 0 or self.x +self.r >= 800:
+            self.vx = -self.vx/1.2
+            self.vy = self.vy/1.1
+        if self.y - self.r <= 0 or self.y +self.r >= 600:
+            self.vy = -self.vy/1.2
+            self.vx = self.vx / 1.1
+        if self.x <= 0 or self.x >= 800 or self.y <= 0 or self.y >= 600:
+            self.vx = 0
+            self.vy = 0
+            self.g=0
     def draw(self):
         pygame.draw.circle(
             self.screen,
@@ -79,32 +84,34 @@ class Ball:
 
 
 class Gun:
-    def __init__(self, screen, surface, x1, y1):
+    def __init__(self, screen, surface):
         self.screen = screen
         self.surface = surf
         self.f2_power = 10
         self.f2_on = 0
         self.an = 1
         self.color = GREEN
-        self.x1 = x1
-        self.y1 = y1
-
+        self.x = x
+        self.y = y
+        self.length = 30
+        self.width = 5
     def fire2_start(self, event):
         self.f2_on = 1
 
     def fire2_end(self, event):
         """Выстрел мячом.
-
         Происходит при отпускании кнопки мыши.
         Начальные значения компонент скорости мяча vx и vy зависят от положения мыши.
         """
+        (x_mouse, y_mouse) = pygame.mouse.get_pos()
         new_ball = Ball(self.screen, 100, 580, 20, 10, 10 * math.tan(self.an), RED)
-        new_ball.r = 15
-        self.an = math.atan2((event.pos[1]-new_ball.y), (event.pos[0]-new_ball.x))
+        self.an = math.atan2((-y_mouse + self.y), (x_mouse - self.x))
         new_ball.vx = self.f2_power * math.cos(self.an)
         new_ball.vy = - self.f2_power * math.sin(self.an)
         self.f2_on = 0
         self.f2_power = 10
+        self.length = 30
+        self.width = 5
 
     def targetting(self, event):
         """Прицеливание. Зависит от положения мыши."""
@@ -115,26 +122,32 @@ class Gun:
         else:
             self.color = GREEN
 
-    def draw(self, screen, surface, x1, y1):
-        self.screen = screen
-        self.an = math.atan((y1 - 400) / (x1 - 20))
-        self.surface = surface
-        pygame.draw.rect(
-            self.surface,
-            BLACK,
-            (0, 500, 80, 20))
-        pygame.transform.scale(self.surface, (10 * self.f2_power, 20))
-        pygame.transform.rotate(self.surface, self.an)
-        self.screen.blit(self.surface, (20, 400))
+    def draw(self):
+        (x_mouse, y_mouse) = pygame.mouse.get_pos()
+        self.x = 0
+        self.y = 450
+        self.an = math.atan2((-y_mouse + self.y), (x_mouse - self.x))
+        length_up = self.length + self.f2_power
+        width_half = self.width / 2
+        pygame.draw.polygon(self.screen, self.color,
+                            ((self.x - width_half * math.sin(self.an),
+                              self.y - width_half * math.cos(self.an)),
+                             (self.x + width_half * math.sin(self.an),
+                              self.y + width_half * math.cos(self.an)),
+                             (self.x + width_half * math.sin(self.an) + length_up * math.cos(self.an),
+                              self.y + width_half * math.cos(self.an) - length_up * math.sin(self.an)),
+                             (self.x - width_half * math.sin(self.an) + length_up * math.cos(self.an),
+                              self.y - width_half * math.cos(self.an) - length_up * math.sin(self.an))))
 
     def power_up(self):
+        global POWER
         if self.f2_on:
             if self.f2_power < 100:
                 self.f2_power += 1
+                POWER = self.f2_power
             self.color = RED
         else:
             self.color = GREEN
-
 
 class Target(Ball):
     def __init__(self, screen, x, y, r, vx, vy, color, points):
@@ -171,7 +184,6 @@ class Target(Ball):
 class Bullet(Ball):
     def __init__(self, screen, x, y, r, vx, vy, color):
      super().__init__(screen, x, y, r, vx, vy, color)
-     self.points = points
      self.screen = screen
 
     def draw(self):
@@ -182,7 +194,10 @@ class Bullet(Ball):
                 self.r
             )
 
+
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+
 points = 0
 
 Targets = []
@@ -197,14 +212,22 @@ for i in range(randint(3, 6)):
     Targets.append(my_ball)
 l = len(Targets)
 bullet = 0
+s = 0
+score = 0
+Bullets = []
 clock = pygame.time.Clock()
-gun = Gun(screen, surf, 0, 0)
+gun = Gun(screen, surf)
 finished = False
 
 while not finished:
     screen.fill(WHITE)
+    f1 = pygame.font.Font(None, 50)
+    text1 = f1.render(str(score), True, (180, 0, 0))
+    text2 = f1.render('score:', True, (180, 0, 0))
+    screen.blit(text1, (120, 50))
+    screen.blit(text2, (10, 50))
     x1, y1 = pygame.mouse.get_pos()
-    gun.draw(screen, surf, x1, y1)
+    gun.draw()
     for i in range(l):
         Ball.draw(Targets[i])
         Ball.move(Targets[i])
@@ -217,19 +240,44 @@ while not finished:
             gun.fire2_start(event)
         elif event.type == pygame.MOUSEBUTTONUP:
             gun.fire2_end(event)
+            (x_mouse, y_mouse) = pygame.mouse.get_pos()
+            angle = math.atan2((-y_mouse + 450), (x_mouse - 20))
+            new_ball1 = Ball(screen, 20, 450, 20, POWER/2.5 * math.cos(angle),  POWER/2.5 * math.sin(angle), GAME_COLORS[randint(0, 5)])
+            Bullets.append(new_ball1)
+            s = len(Bullets)
+    for i in range(s):
+        Ball.draw(Bullets[i])
+        Ball.move(Bullets[i])
+        for i in range(l):
+           for j in range(s):
+            if Targets[i].hittest(Bullets[j]) == True:
+                Targets.pop(i)
+                score += 1
+                x = randint(100, 700)
+                y = randint(100, 500)
+                r = randint(30, 50)
+                vx = randint(-2, 2)
+                vy = randint(-10, 10)
+                COLOR = GAME_COLORS[randint(0, 5)]
+                my_ball = Target(screen, x, y, r, vx, vy, COLOR, points)
+                Targets.append(my_ball)
 
-        #for i in range(l):
-            #if Targets[i].hittest(new_ball) == True:
-                #Targets.pop(i)
-                #x = randint(100, 700)
-                #y = randint(100, 500)
-                #r = randint(30, 50)
-                #vx = randint(-2, 2)
-                #vy = randint(-10, 10)
-                #COLOR = GAME_COLORS[randint(0, 5)]
-                #my_ball = Target(screen, x, y, r, vx, vy, COLOR, points)
-                #Targets.append(my_ball)
     gun.power_up()
+    for i in range(s):
+        if Bullets[i].vx == 0:
+            Bullets.pop(i)
+    for i in range(l):
+        if Targets[i].vx == 0:
+            Targets.pop(i)
+            score += 1
+            x = randint(100, 700)
+            y = randint(100, 500)
+            r = randint(30, 50)
+            vx = randint(-2, 2)
+            vy = randint(-10, 10)
+            COLOR = GAME_COLORS[randint(0, 5)]
+            my_ball = Target(screen, x, y, r, vx, vy, COLOR, points)
+            Targets.append(my_ball)
     pygame.display.update()
 
     clock.tick(FPS)
